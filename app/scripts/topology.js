d9f1d9001e1d7ed18759dce066b5e3523a72d9ca
Franziska Hollauf <zissxd@gmail.com>
2019-12-21 19:12:33 +0100

    landscape_net finished

--- app/scripts/topology.js
+++ app/scripts/topology.js
@@ -75,315 +75,310 @@ export default function topology() {
       },
       {
         'cityId': '13',
-        'x': 60,
+        'x': 68,
         'y': 59,
         'isIntermediatePoint': true
       },
       {
         'cityId': '14',
-        'x': 58,
-        'y': 57,
-        'isIntermediatePoint': false
-      },
-      {
-        'cityId': '15',
-        'x': 74,
-        'y': 46,
-        'isIntermediatePoint': false
-      },
-      {
-        'cityId': '16',
-        'x': 60,
-        'y': 46,
-        'isIntermediatePoint': false
-      },
-      {
-        'cityId': '17',
-        'x': 57,
-        'y': 45,
-        'isIntermediatePoint': false
-      },
-      {
-        'cityId': '18',
-        'x': 73,
-        'y': 44,
-        'isIntermediatePoint': true
-      },
-      {
-        'cityId': '19',
-        'x': 72,
-        'y': 42,
-        'isIntermediatePoint': false
-      },
-      {
-        'cityId': '20',
-        'x': 70,
-        'y': 43,
-        'isIntermediatePoint': true
-      },
-      {
-        'cityId': '21',
-        'x': 68,
-        'y': 44,
-        'isIntermediatePoint': true
-      },
-      {
-        'cityId': '22',
-        'x': 66,
-        'y': 45,
-        'isIntermediatePoint': true
-      },
-      {
-        'cityId': '23',
-        'x': 64,
-        'y': 46,
-        'isIntermediatePoint': false
-      },
-      {
-        'cityId': '24',
         'x': 65,
         'y': 55,
         'isIntermediatePoint': false
       },
       {
+        'cityId': '15',
+        'x': 73,
+        'y': 42,
+        'isIntermediatePoint': false
+      },
+      {
+        'cityId': '16',
+        'x': 68,
+        'y': 43,
+        'isIntermediatePoint': false
+      },
+      {
+        'cityId': '17',
+        'x': 63,
+        'y': 40,
+        'isIntermediatePoint': false
+      },
+      {
+        'cityId': '18',
+        'x': 72.5,
+        'y': 34,
+        'isIntermediatePoint': true
+      },
+      {
+        'cityId': '19',
+        'x': 72,
+        'y': 27,
+        'isIntermediatePoint': false
+      },
+      {
+        'cityId': '20',
+        'x': 68,
+        'y': 28,
+        'isIntermediatePoint': true
+      },
+      {
+        'cityId': '21',
+        'x': 64,
+        'y': 32,
+        'isIntermediatePoint': true
+      },
+      {
+        'cityId': '22',
+        'x': 61,
+        'y': 34,
+        'isIntermediatePoint': true
+      },
+      {
+        'cityId': '23',
+        'x': 58,
+        'y': 35,
+        'isIntermediatePoint': false
+      },
+      {
+        'cityId': '24',
+        'x': 60,
+        'y': 50,
+        'isIntermediatePoint': false
+      },
+      {
         'cityId': '25',
-        'x': 80.5,
-        'y': 71,
+        'x': 56,
+        'y': 48,
         'isIntermediatePoint': false
       },
       {
         'cityId': '26',
-        'x': 75,
-        'y': 67,
+        'x': 49,
+        'y': 48,
         'isIntermediatePoint': false
       },
       {
         'cityId': '27',
-        'x': 80,
-        'y': 60,
-        'isIntermediatePoint': true
+        'x': 47,
+        'y': 49,
+        'isIntermediatePoint': false
       },
       {
         'cityId': '28',
-        'x': 79,
-        'y': 52,
-        'isIntermediatePoint': true
+        'x': 52,
+        'y': 40,
+        'isIntermediatePoint': false
       },
       {
         'cityId': '29',
-        'x': 78.5,
-        'y': 46,
+        'x': 52.5,
+        'y': 33,
         'isIntermediatePoint': false
       },
       {
         'cityId': '30',
-        'x': 74.5,
-        'y': 59,
-        'isIntermediatePoint': true
+        'x': 52,
+        'y': 55,
+        'isIntermediatePoint': false
       },
       {
         'cityId': '31',
-        'x': 74,
-        'y': 51,
+        'x': 55,
+        'y': 59,
         'isIntermediatePoint': false
       },
       {
         'cityId': '32',
-        'x': 71.5,
-        'y': 64.8,
+        'x': 53.5,
+        'y': 67,
         'isIntermediatePoint': false
       },
       {
         'cityId': '33',
-        'x': 79,
-        'y': 74,
+        'x': 52,
+        'y': 77,
         'isIntermediatePoint': false
       },
       {
         'cityId': '34',
-        'x': 78,
-        'y': 78,
+        'x': 47,
+        'y': 59,
         'isIntermediatePoint': false
       },
       {
         'cityId': '35',
-        'x': 80.5,
-        'y': 71,
+        'x': 47.5,
+        'y': 39,
         'isIntermediatePoint': false
       },
       {
         'cityId': '36',
-        'x': 75,
-        'y': 67,
-        'isIntermediatePoint': false
-      },
-      {
-        'cityId': '37',
-        'x': 80,
-        'y': 60,
+        'x': 48,
+        'y': 32,
         'isIntermediatePoint': true
       },
       {
+        'cityId': '37',
+        'x': 44,
+        'y': 31,
+        'isIntermediatePoint': false
+      },
+      {
         'cityId': '38',
-        'x': 79,
-        'y': 52,
+        'x': 41,
+        'y': 31,
         'isIntermediatePoint': true
       },
       {
         'cityId': '39',
-        'x': 78.5,
-        'y': 46,
+        'x': 38.5,
+        'y': 28.5,
         'isIntermediatePoint': false
       },
       {
         'cityId': '40',
-        'x': 74.5,
-        'y': 59,
+        'x': 36,
+        'y': 35,
         'isIntermediatePoint': true
       },
       {
         'cityId': '41',
-        'x': 74,
-        'y': 51,
-        'isIntermediatePoint': false
+        'x': 44,
+        'y': 40,
+        'isIntermediatePoint': true
       },
       {
         'cityId': '42',
-        'x': 71.5,
-        'y': 64.8,
-        'isIntermediatePoint': false
+        'x': 41,
+        'y': 42,
+        'isIntermediatePoint': true
       },
       {
         'cityId': '43',
-        'x': 79,
-        'y': 74,
-        'isIntermediatePoint': false
+        'x': 36,
+        'y': 44,
+        'isIntermediatePoint': true
       },
       {
         'cityId': '44',
-        'x': 78,
-        'y': 78,
+        'x': 32,
+        'y': 43.5,
         'isIntermediatePoint': false
       },
       {
         'cityId': '45',
-        'x': 80.5,
-        'y': 71,
+        'x': 29.5,
+        'y': 49,
         'isIntermediatePoint': false
       },
       {
         'cityId': '46',
-        'x': 75,
-        'y': 67,
+        'x': 26,
+        'y': 42,
         'isIntermediatePoint': false
       },
       {
         'cityId': '47',
-        'x': 80,
-        'y': 60,
-        'isIntermediatePoint': true
+        'x': 23,
+        'y': 39,
+        'isIntermediatePoint': false
       },
       {
         'cityId': '48',
-        'x': 79,
-        'y': 52,
-        'isIntermediatePoint': true
+        'x': 22,
+        'y': 44,
+        'isIntermediatePoint': false
       },
       {
         'cityId': '49',
-        'x': 78.5,
-        'y': 46,
+        'x': 28,
+        'y': 57,
         'isIntermediatePoint': false
       },
       {
         'cityId': '50',
-        'x': 74.5,
-        'y': 59,
-        'isIntermediatePoint': true
+        'x': 34,
+        'y': 60,
+        'isIntermediatePoint': false
       },
       {
         'cityId': '51',
-        'x': 74,
-        'y': 51,
-        'isIntermediatePoint': false
+        'x': 36,
+        'y': 57,
+        'isIntermediatePoint': true
       },
       {
         'cityId': '52',
-        'x': 71.5,
-        'y': 64.8,
-        'isIntermediatePoint': false
+        'x': 39,
+        'y': 53,
+        'isIntermediatePoint': true
       },
       {
         'cityId': '53',
-        'x': 79,
-        'y': 74,
-        'isIntermediatePoint': false
+        'x': 43,
+        'y': 51,
+        'isIntermediatePoint': true
       },
       {
         'cityId': '54',
-        'x': 78,
-        'y': 78,
+        'x': 31,
+        'y': 68,
         'isIntermediatePoint': false
       },
       {
         'cityId': '55',
-        'x': 80.5,
-        'y': 71,
+        'x': 33,
+        'y': 75,
         'isIntermediatePoint': false
       },
       {
         'cityId': '56',
-        'x': 75,
-        'y': 67,
+        'x': 33,
+        'y': 85,
         'isIntermediatePoint': false
       },
       {
         'cityId': '57',
-        'x': 80,
-        'y': 60,
-        'isIntermediatePoint': true
+        'x': 38,
+        'y': 71,
+        'isIntermediatePoint': false
       },
       {
         'cityId': '58',
-        'x': 79,
-        'y': 52,
+        'x': 37,
+        'y': 83
+        ,
         'isIntermediatePoint': true
       },
       {
         'cityId': '59',
-        'x': 78.5,
-        'y': 46,
-        'isIntermediatePoint': false
+        'x': 41,
+        'y': 68,
+        'isIntermediatePoint': true
       },
       {
         'cityId': '60',
-        'x': 74.5,
-        'y': 59,
+        'x': 43,
+        'y': 65,
         'isIntermediatePoint': true
       },
       {
         'cityId': '61',
-        'x': 74,
-        'y': 51,
-        'isIntermediatePoint': false
+        'x': 44,
+        'y': 62,
+        'isIntermediatePoint': true
       },
       {
         'cityId': '62',
-        'x': 71.5,
-        'y': 64.8,
-        'isIntermediatePoint': false
+        'x': 42,
+        'y': 81,
+        'isIntermediatePoint': true
       },
       {
         'cityId': '63',
-        'x': 79,
-        'y': 74,
-        'isIntermediatePoint': false
-      },
-      {
-        'cityId': '64',
-        'x': 78,
-        'y': 78,
-        'isIntermediatePoint': false
+        'x': 47,
+        'y': 79,
+        'isIntermediatePoint': true
       }
     ],
     'connections': [
@@ -414,6 +409,222 @@ export default function topology() {
       }, {
         'start': '8',
         'end': '9'
+      }, {
+        'start': '6',
+        'end': '12'
+      }, {
+        'start': '6',
+        'end': '10'
+      }, {
+        'start': '10',
+        'end': '11'
+      }, {
+        'start': '11',
+        'end': '9'
+      }, {
+        'start': '15',
+        'end': '9'
+      }, {
+        'start': '12',
+        'end': '13'
+      }, {
+        'start': '13',
+        'end': '14'
+      }, {
+        'start': '14',
+        'end': '17'
+      }, {
+        'start': '14',
+        'end': '24'
+      }, {
+        'start': '24',
+        'end': '25'
+      }, {
+        'start': '15',
+        'end': '16'
+      }, {
+        'start': '16',
+        'end': '17'
+      }, {
+        'start': '15',
+        'end': '18'
+      }, {
+        'start': '18',
+        'end': '19'
+      }, {
+        'start': '20',
+        'end': '19'
+      }, {
+        'start': '20',
+        'end': '21'
+      }, {
+        'start': '21',
+        'end': '22'
+      }, {
+        'start': '22',
+        'end': '23'
+      }, {
+        'start': '17',
+        'end': '23'
+      }, {
+        'start': '23',
+        'end': '29'
+      }, {
+        'start': '23',
+        'end': '28'
+      }, {
+        'start': '28',
+        'end': '25'
+      }, {
+        'start': '25',
+        'end': '31'
+      }, {
+        'start': '25',
+        'end': '26'
+      }, {
+        'start': '25',
+        'end': '30'
+      }, {
+        'start': '30',
+        'end': '31'
+      }, {
+        'start': '30',
+        'end': '27'
+      }, {
+        'start': '26',
+        'end': '27'
+      }, {
+        'start': '28',
+        'end': '27'
+      }, {
+        'start': '28',
+        'end': '35'
+      }, {
+        'start': '28',
+        'end': '29'
+      }, {
+        'start': '29',
+        'end': '35'
+      }, {
+        'start': '29',
+        'end': '36'
+      }, {
+        'start': '27',
+        'end': '34'
+      }, {
+        'start': '31',
+        'end': '34'
+      }, {
+        'start': '31',
+        'end': '32'
+      }, {
+        'start': '32',
+        'end': '33'
+      }, {
+        'start': '33',
+        'end': '63'
+      }, {
+        'start': '36',
+        'end': '37'
+      }, {
+        'start': '37',
+        'end': '38'
+      }, {
+        'start': '38',
+        'end': '39'
+      }, {
+        'start': '39',
+        'end': '40'
+      }, {
+        'start': '40',
+        'end': '44'
+      },{
+        'start': '44',
+        'end': '43'
+      }, {
+        'start': '42',
+        'end': '43'
+      }, {
+        'start': '41',
+        'end': '42'
+      }, {
+        'start': '35',
+        'end': '41'
+      }, {
+        'start': '44',
+        'end': '46'
+      }, {
+        'start': '44',
+        'end': '45'
+      }, {
+        'start': '46',
+        'end': '45'
+      }, {
+        'start': '46',
+        'end': '47'
+      }, {
+        'start': '47',
+        'end': '48'
+      }, {
+        'start': '48',
+        'end': '46'
+      }, {
+        'start': '45',
+        'end': '49'
+      }, {
+        'start': '49',
+        'end': '50'
+      }, {
+        'start': '50',
+        'end': '51'
+      }, {
+        'start': '50',
+        'end': '54'
+      }, {
+        'start': '55',
+        'end': '54'
+      },{
+        'start': '50',
+        'end': '57'
+      }, {
+        'start': '51',
+        'end': '52'
+      }, {
+        'start': '52',
+        'end': '53'
+      }, {
+        'start': '53',
+        'end': '27'
+      }, {
+        'start': '55',
+        'end': '57'
+      }, {
+        'start': '55',
+        'end': '56'
+      }, {
+        'start': '56',
+        'end': '57'
+      }, {
+        'start': '56',
+        'end': '58'
+      }, {
+        'start': '57',
+        'end': '59'
+      }, {
+        'start': '59',
+        'end': '60'
+      }, {
+        'start': '60',
+        'end': '61'
+      }, {
+        'start': '61',
+        'end': '34'
+      }, {
+        'start': '58',
+        'end': '62'
+      }, {
+        'start': '62',
+        'end': '63'
       }
     ]
   };
