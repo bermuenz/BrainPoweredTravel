0299d65ca8007a11cfa148edb7c37550c2554529
unknown <amplejoe@gmail.com>
2019-12-21 14:56:07 +0100

    andi scene

---
+++ app/scripts/scenes/andi.js
@@ -0,0 +1,33 @@
+export default class Andi extends Phaser.Scene {
+  /**
+   *  A sample Game scene, displaying the Phaser logo.
+   *
+   *  @extends Phaser.Scene
+   */
+  constructor() {
+    super({key: 'Andi'});
+  }
+
+  /**
+   *  Called when a scene is initialized. Method responsible for setting up
+   *  the game objects of the scene.
+   *
+   *  @protected
+   *  @param {object} data Initialization parameters.
+   */
+  create(/* data */) {
+    //  TODO: Replace this content with really cool game code here :)
+
+  }
+
+  /**
+   *  Called when a scene is updated. Updates to game logic, physics and game
+   *  objects are handled here.
+   *
+   *  @protected
+   *  @param {number} t Current internal clock time.
+   *  @param {number} dt Time elapsed since last update.
+   */
+  update(/* t, dt */) {
+  }
+}
