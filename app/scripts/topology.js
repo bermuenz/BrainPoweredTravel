7b08b6047a49394e72b2a6a3b9acd700e5f211a3
Franziska Hollauf <zissxd@gmail.com>
2019-12-21 14:59:12 +0100

    added a few new points

--- app/scripts/topology.js
+++ app/scripts/topology.js
@@ -12,12 +12,82 @@ export default function topology() {
         'x': 75,
         'y': 75,
         'isIntermediatePoint': false
+      },
+      {
+        'cityId': '3',
+        'x': 79,
+        'y': 74,
+        'isIntermediatePoint': false
+      },
+      {
+        'cityId': '4',
+        'x': 78,
+        'y': 78,
+        'isIntermediatePoint': false
+      },
+      {
+        'cityId': '5',
+        'x': 80.5,
+        'y': 71,
+        'isIntermediatePoint': false
+      },
+      {
+        'cityId': '6',
+        'x': 75,
+        'y': 67,
+        'isIntermediatePoint': false
+      },
+      {
+        'cityId': '7',
+        'x': 80,
+        'y': 60,
+        'isIntermediatePoint': true
+      },
+      {
+        'cityId': '8',
+        'x': 79,
+        'y': 52,
+        'isIntermediatePoint': true
+      },
+      {
+        'cityId': '9',
+        'x': 78.5,
+        'y': 46,
+        'isIntermediatePoint': false
       }
     ],
     'connections': [
       {
         'start': 'Melbourne',
         'end': 'Alice Springs'
+      },
+      {
+        'start': 'Melbourne',
+        'end': '4'
+      },{
+        'start': '4',
+        'end': 'Alice Springs'
+      },{
+        'start': 'Melbourne',
+        'end': '3'
+      },{
+        'start': '3',
+        'end': 'Alice Springs'
+      },{
+        'start': '6',
+        'end': 'Alice Springs'
+      },{
+        'start': 'Melbourne',
+        'end': '5'
+      },{
+        'start': '5',
+        'end': '7'
+      },{
+        'start': '7',
+        'end': '8'
+      },{
+        'start': '8',
+        'end': '9'
       }
     ]
   };
