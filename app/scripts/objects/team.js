4144656192531c9c3b9457b6327138769655018a
phmoll <philipp.moll@aau.at>
2019-12-21 16:39:46 +0100

    Replace token images with flags

--- app/scripts/objects/team.js
+++ app/scripts/objects/team.js
@@ -13,20 +13,20 @@ export default class Team {
     this.ecoFootprint = new EcoFootprint(scene, this);
 
     // Create the token
-    this.token = new Phaser.GameObjects.Sprite(scene, 0, 0, teamId == 0 ? 'figure_orange-10' : 'figure_green-11');
+    this.token = new Phaser.GameObjects.Sprite(scene, 0, 0, teamId == 0 ? 'figure_yellow-11' : 'figure_blue-10');
     scene.add.existing(this.token);
-    this.token.setOrigin(0.5, 1);
+    this.token.setOrigin(teamId == 0 ? 0.8 : 0.2, 1);
     this.token.setScale(0.6, 0.6);
-    this.token.setPosition(this.currentCity.x, this.currentCity.y + 10);
+    this.token.setPosition(this.currentCity.x, this.currentCity.y + 3);
 
     scene.tweens.add({
         targets: this.token,
-        scale: 0.62,
+        scale: 0.75,
         duration: 500,
         ease: 'Linear',
         yoyo: -1,
         repeat: -1
-    })
+    });
 
   }
 
