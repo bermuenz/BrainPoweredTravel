f025e837bcbd8b26ac45f236773b8f823120fdc2
anleibetsed <you@example.com>
2019-12-22 07:16:01 +0100

    fix quizcard

--- app/scripts/objects/quizcard.js
+++ app/scripts/objects/quizcard.js
@@ -9,8 +9,9 @@ export default class Quizcard extends Phaser.GameObjects.Container {
     this.scene = scene;
     this.riddle = riddle;
 
+
     this.riddle = {
-      'dID' : '3',
+      'dID' : '31',
       'qText' : 'How many pieces are on a chessboard at the beginning of a game?',
       'qOptions' : ['32','28', '34','30'],
       'aText' : '32: Each opponent has 1 king, 1 queen, 2 rooks, 2 bishops, 2 knights, and 8 pawns.',
@@ -28,73 +29,113 @@ export default class Quizcard extends Phaser.GameObjects.Container {
 
 
     // bg
-    let bg = this.scene.add.sprite(centerX, centerY, 'quizcard').setOrigin(0);
-    bg.setOrigin(0.5, 0.5);
-    this.add(bg);
-    // this.setWidth(bg.width);
-    // this.setHeight(bg.height);
+    this.bg = this.scene.add.sprite(centerX, centerY, 'quizcard').setOrigin(0);
+    this.bg.setOrigin(0.5, 0.5);
+    this.add(this.bg);
 
+
+    // front
+    this.createCardFront();
+
+    this.setSize(this.bg.width, this.bg.height);
+
+    this.scene.add.existing(this);
+
+  }
+
+  createCardFront(cacheKey, offsets) {
+    let bgCenterX = this.bg.getCenter().x;
+    let bgCenterY = this.bg.getCenter().y;
+    let lineGap = 65;
+    let middleOffset = 100;
 
     // score etc
-    let txtStyleTop = {
-      fontSize: 18,
-      align: "center",
-      fontFamily: '"Roboto Condensed"',
-      color: "white"
-    };
-    let offsetX = 65;
-    let offsetY = 65;
-    this.difficultyText = this.scene.add.text((bg.x - bg.width/2) + offsetX, (bg.y - bg.height/2) + offsetY, `Difficulty: ${this.riddle.difficulty}`, txtStyleTop);
-    this.timeRemainingTxt = this.scene.add.text((bg.x + bg.width/2) - offsetX * 2.1, (bg.y - bg.height/2) + offsetY, `Time: `, txtStyleTop);
-    // this.pointsTxt = this.scene.add.text(bg.width - 100, 50, `Difficulty: ${this.riddle.difficulty}`, txtStyleTop);
-    this.add(this.difficultyText);
-    this.add(this.timeRemainingTxt);
+    let currentY = this.placeScore();
 
     // question
-    let currentOffsetY = 0.45;
+    // let currentOffsetY = 0.45;
     let txtStyle = {
       fontSize: 22,
       align: "center",
       fontFamily: '"Roboto Condensed"',
       color: "white",
       wordWrap: {
-        width: bg.width - (2 * 60),
+        width: this.bg.width - (2 * 60),
         useAdvancedWrap: true
       }
     };
-    this.text = this.scene.add.text(centerX, centerY * currentOffsetY, this.riddle.qText, txtStyle);
+    this.text = this.scene.add.text(bgCenterX, currentY + lineGap * 0.7, this.riddle.qText, txtStyle);
+    currentY += (lineGap * 1.5);
     this.text.setOrigin(0.5, 0.5);
     // this.text.setShadow(10, 10, "#FFFFFF", 10, true, true);
     this.add(this.text);
-    currentOffsetY += 0.10;
-    let img = "riddles/q" + this.riddle.dID;
+
     if (this.riddle['qOptions']) {
-      let middleOffset = 70;
       for (let i=0; i < this.riddle['qOptions'].length; i++) {
         // let currentY = centerY * (currentOffsetY + i * 0.10);
-        let currentY = centerY + (i%3 === 0 ? -80 : -50);
-        let currentX = centerX + (i%2 === 0 ? -middleOffset : middleOffset);
-        console.log(centerX);
-        console.log(currentX);
-        let tx = this.scene.add.text(currentX, currentY, i +": " + this.riddle['qOptions'][i], txtStyle);
+        let x = bgCenterX + (i%2 === 0 ? -middleOffset : middleOffset);
+        let y = currentY + (i%3 === 0 ? 0: (lineGap * 0.8));
+        // currentY += scoreOffset.y;
+        let tx = this.scene.add.text(x, y, i +": " + this.riddle['qOptions'][i], txtStyle);
         tx.setOrigin(0.5, 0.5);
         this.add(tx);
       }
+      currentY += (3*lineGap);
     }
-    if (this.scene.textures.exists(img)) {
-      this.img = this.scene.add.sprite(centerX, centerY * 1.3, img).setOrigin(0);
+    else currentY += (2 * lineGap);
+
+    let img = "riddles/q" + this.riddle.dID;
+    this.placeImg(img, bgCenterX, currentY);
+
+  }
+
+  placeScore() {
+    let bgCenterX = this.bg.getCenter().x;
+    let bgCenterY = this.bg.getCenter().y;
+    let lineGap = 65;
+    let middleOffset = 100;
+    let txtStyleTop = {
+      fontSize: 18,
+      align: "center",
+      fontFamily: '"Roboto Condensed"',
+      color: "white"
+    };
+    let scoreOffset = {x: this.bg.width/2 - lineGap, y: this.bg.height/2 - lineGap};
+    this.difficultyText = this.scene.add.text(0, 0, `Difficulty: ${this.riddle.difficulty}`, txtStyleTop);
+    this.timeRemainingTxt = this.scene.add.text(0, 0, `Time: `, txtStyleTop);
+    this.difficultyText.setPosition(bgCenterX - scoreOffset.x, bgCenterY - scoreOffset.y);
+    this.timeRemainingTxt.setPosition(bgCenterX + (scoreOffset.x + lineGap/3 - this.timeRemainingTxt.width - this.timeRemainingTxt.width) , bgCenterY - scoreOffset.y);
+    // this.pointsTxt = this.scene.add.text(this.bg.width - 100, 50, `Difficulty: ${this.riddle.difficulty}`, txtStyleTop);
+    this.add(this.difficultyText);
+    this.add(this.timeRemainingTxt);
+    return bgCenterY - scoreOffset.y;
+  }
+
+  placeImg(cacheKey, x, y, ease = false) {
+
+    if (this.scene.textures.exists(cacheKey)) {
+      if (this.img) {
+        this.img.destroy();
+      }
+      this.img = this.scene.add.sprite(x, y, cacheKey);
       this.img.setOrigin(0.5, 0.5);
-      this.img.setScale(0.4, 0.4);
+      this.img.setScale(0.35, 0.35);
       this.add(this.img);
     }
 
-
-    // answer
-
-    this.setSize(bg.width, bg.height);
-
-    this.scene.add.existing(this);
-
+    if (ease) {
+      let origScale = this.img.scaleX;
+      this.img.setScale(0, this.img.scaleY);
+      let easin = this.scene.tweens.add({
+        targets: this.img,
+        scaleX: origScale,
+        ease: 'Linear',
+        duration: 300,
+        onComplete: () => {
+          this.scene.tweens.remove(easin);
+        }
+      });
+    }
   }
 
   getRemainingTime() {
@@ -114,18 +155,32 @@ export default class Quizcard extends Phaser.GameObjects.Container {
       duration: duration,
       onComplete: () => {
         this.scene.tweens.remove(fadeout);
-        let fadein = this.scene.tweens.add({
-          targets: objs,
-          alpa: 1.00,
-          ease: 'Linear',
-          duration: duration,
-          onComplete: () => {
-            this.scene.tweens.remove(fadein);
-          }
-        });
       }
     });
+  }
 
+  fadeinObjects(objs) {
+    let centerX = this.scene.cameras.main.width  * 0.5;
+    let centerY = this.scene.cameras.main.height  * 0.5;
+    let duration = 20;
+    let fadein = this.scene.tweens.add({
+      targets: objs,
+      alpa: 1.00,
+      ease: 'Linear',
+      duration: duration,
+      onComplete: () => {
+        this.scene.tweens.remove(fadein);
+      }
+    });
+  }
+
+  removeObjects(objs) {
+    let idxs = [];
+    for (let o of objs) {
+      o.destroy();
+      this.remove(o);
+      o = null;
+    }
   }
 
   flip(isAnswered = false) {
@@ -142,8 +197,18 @@ export default class Quizcard extends Phaser.GameObjects.Container {
           repeat: 0,
           yoyo: false,
           onComplete: () => {
-            if (this.text.text === this.riddle.qText) this.text.text = this.riddle.aText;
-            else this.text.text = this.riddle.qText;
+            let img = null;
+            if (this.text.text === this.riddle.qText) {
+              this.text.text = this.riddle.aText;
+              img = "riddles/a" + this.riddle.dID;
+            }
+            else {
+              this.text.text = this.riddle.qText;
+              img = "riddles/q" + this.riddle.dID;
+              this.placeScore();
+              this.fadeoutObjects([this.difficultyText, this.timeRemainingTxt]);
+            }
+            this.placeImg(img, this.img.x, this.img.y, true);
             this.scene.tweens.remove(flipA);
             let flipB = this.scene.tweens.add({
                 targets: obj,
@@ -163,11 +228,7 @@ export default class Quizcard extends Phaser.GameObjects.Container {
   }
 
   onFlipComplete(obj) {
-    console.log("Completed!");
-      // do something, show text, add score etc.
-      // if (card.frameName == 'HeartQueen') {
-      //     // ?
-      // };
+
   }
 
   /**
@@ -175,7 +236,7 @@ export default class Quizcard extends Phaser.GameObjects.Container {
    */
   update() {
     //console.log(this.getRemainingTime(), this.computePoints());
-    this.timeRemainingTxt.text = 'Time: ' + this.getRemainingTime();
+    if(this.timeRemainingTxt) this.timeRemainingTxt.setText('Time: ' + this.getRemainingTime());
     // this.pointsTxt.text = this.computePoints();
   }
 
